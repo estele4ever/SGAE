3306d361527dec3d8f8e25ee21eb49f23e629e65
estele <estelengnemie@gmail.com>
2025-04-09 15:34:19 +0100

    le dernier travail de estele sur la machine de estele

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("vous etes connecter!") }}
-                </div>
-               
+                </div>               
 
             </div>
             
